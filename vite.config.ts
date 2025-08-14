d4b8ac1d7c3f2fff86315d6588183e090e4c09a7
rubyhuynhdotcom <rubyhuynhdotcom@gmail.com>
2025-08-14 19:44:57 +0700

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/kingbull-insight-hub/',   // <-- QUAN TRỌNG
+})
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
